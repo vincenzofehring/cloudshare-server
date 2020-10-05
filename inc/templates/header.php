a860d132331e75b81b78e3ad8730cdf0d9862bb3
vincenzofehring <vincenzo.fehring@me.com>
2020-10-05 17:38:24 -0500

    Small css and path fixes

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -3,12 +3,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>CloudShare</title>
-    <base href="/"<?php echo($WEBROOT); ?>" />
+    <base href="<?php echo($WEBROOT); ?>/" />
     <link rel="stylesheet" type="text/css" href="css/default.css" />
 </head>
 <body>
 <?php
-    echo('<h1><a id="cloudshare-logo" href="/' . $WEBROOT . '"><span>CloudShare</span></a></h1>');
+    echo('<h1><a id="cloudshare-logo" href="' . $WEBROOT . '"><span>CloudShare</span></a></h1>');
 
 
     // Check if already configured. Otherwise start configuration wizard
--- log/index.php
+++ log/index.php
@@ -22,7 +22,7 @@
 
 
 
-require_once('../inf/lib_base.php');
+require_once('../inc/lib_base.php');
 
 CS_UTIL::showheader();
 CS_LOG::show();
